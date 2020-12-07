4fb0376bc7e04598939192eac972fa72c3953d18
VenusCW <mz.ve513@gmail.com>
2020-12-07 18:06:02 -0500

    TDD commit

---
+++ script.js
@@ -0,0 +1,38 @@
+describe('convertWordsToLowercase', function () {
+	it(' All lowercase Adam', function () {
+		expect(Adam).toEqual('adam');
+    });
+    
+
+
+describe('translateEnglishToPigLatin',function(string){
+it("wordsStartingWithVowels a,", function(){
+expect(translate('adam')).toEqual('adamway')
+});
+});
+
+it("wordsStartingWithVowels e add way,", function(){
+    expect(translate('eat')).toEqual('eatway')
+});
+
+it("wordsStartingWithVowels i add way,", function(){
+    expect(translate('igloo')).toEqual('iglooway')
+});
+
+it("wordsStartingConsonant d add ay,", function(){
+    expect(translate('dry')).toEqual('ydray')
+});
+
+it("wordsStartingConsonant f add ay,", function(){
+    expect(translate('fry')).toEqual('yfray')
+});
+it("wordsStartingConsonant f add ay,", function(){
+    expect(translate('friday')).toEqual('idayfray')
+});
+
+it("wordsStartingConsonant c add ay,", function(){
+    expect(translate('crayon')).toEqual('ayoncray')
+});
+
+
+
